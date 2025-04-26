aac45050a2b939ee9b02f9237c39931b131f93ff
Азамат Куспанов <azamat.kuspanov@kmf.kz>
2025-04-26 11:26:16 +0500

    fix gitignore.

---
+++ engine/src/window/win32/window.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <pch.h>
+#include <WindowsX.h>
+#include <tchar.h>
+#include <hidusage.h>
+#include <queue>
+#include "window/window.h"
+#include "events/events.h"
+
+namespace engine
+{
+	class winApiWindow : public baseWindow
+	{
+	private:
+		static void logLastError(const std::string& prefix);
+		static std::mutex hwndTableMu;
+		static std::map<HWND, winApiWindow*> hwndTable;
+		static WNDCLASSEX wndClass;
+		static std::once_flag isWindowClassCreated;
+		static engine::error createWndClassErr;
+		static LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+		static bool handleRawInput(winApiWindow* winApiInst, HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+		static bool handleCloseEvent(winApiWindow* winApiInst, HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+		static bool handleResizeEvent(winApiWindow* winApiInst, HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+		static bool handlePaintEvent(winApiWindow* winApiInst, HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+		void createWindowClass(const std::string& applicationName);
+		void registerInputDevices();
+		void createWindow();
+		void pollRawInput();
+
+		std::mutex mEvenetQueueMu;
+		std::map<key, std::shared_ptr<baseEvent>> mKeyDown;
+		std::queue<std::shared_ptr<baseEvent>> mEventQueue;
+
+		std::string mApplicationName;
+		HDC mHdc;
+		HWND mHWnd;
+		HGLRC mHrc;
+	public:
+		winApiWindow(context ctx, const std::string& name, std::uint32_t width, std::uint32_t heigth, bool isFullscreen, const std::string& applicationName, bool showCursor);
+
+		~winApiWindow();
+		winApiWindow(const winApiWindow& other) = delete;
+		winApiWindow& operator=(const winApiWindow& other) = delete;
+
+		void swapBuffers() const;
+		void toggleCursor();
+		engine::error checkError();
+		void startPolling();
+		engine::error makeOpenglContext();
+		void pollInput();
+		bool isKeyPressed(key);
+	};
+}
---
+++ engine/src/window/win32/windowProc.cpp
@@ -0,0 +1,173 @@
+#include <pch.h>
+#include "window.h"
+
+namespace engine
+{
+	bool winApiWindow::handleRawInput(winApiWindow* winApiInst, HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+	{
+		auto keyCode = fromWinApiMouse(message);
+
+		POINT cursorPos;
+		cursorPos.x = GET_X_LPARAM(lParam);
+		cursorPos.y = GET_Y_LPARAM(lParam);
+
+		switch (message)
+		{
+		case WM_INPUT:
+		{
+			UINT dataSize = 0;
+			if (GetRawInputData(reinterpret_cast<HRAWINPUT>(lParam), RID_INPUT, NULL, &dataSize, sizeof(RAWINPUTHEADER)) == (UINT)-1)
+			{
+				logLastError("GetRawInputData: ");
+			}
+
+			if (dataSize > 0)
+			{
+				void* rawData = alloca(dataSize);
+				if (GetRawInputData(reinterpret_cast<HRAWINPUT>(lParam), RID_INPUT, rawData, &dataSize, sizeof(RAWINPUTHEADER)) == dataSize)
+				{
+					RAWINPUT* raw = static_cast<RAWINPUT*>(rawData);
+
+					std::lock_guard<std::mutex> m(winApiInst->mEvenetQueueMu);
+					if (raw->header.dwType == RIM_TYPEMOUSE)
+					{
+						winApiInst->mEventQueue.push(std::make_shared<mouseMoveEvent>(mouseOffset{ raw->data.mouse.lLastX, raw->data.mouse.lLastY }));
+					}
+					else
+					{
+						const RAWKEYBOARD& kbd = raw->data.keyboard;
+
+						bool isKeyDown = !(kbd.Flags & RI_KEY_BREAK);
+
+						auto keyCode = fromRawKeyboard(raw);
+
+						if (isKeyDown)
+						{
+							if (winApiInst->mKeyDown.find(keyCode) == winApiInst->mKeyDown.end())
+							{
+								winApiInst->mKeyDown[keyCode] = std::make_shared<keyDownEvent>(keyCode);
+							}
+						}
+						else
+						{
+							if (winApiInst->mKeyDown.find(keyCode) != winApiInst->mKeyDown.end())
+							{
+								winApiInst->mKeyDown.erase(keyCode);
+							}
+
+							winApiInst->mEventQueue.push(std::make_shared<keyUpEvent>(keyUpEvent{ keyCode }));
+						}
+					}
+				}
+				else
+				{
+					logLastError("GetRawInputData: ");
+				}
+			}
+
+			return true;
+		}
+		}
+
+		return false;
+	}
+
+	bool winApiWindow::handleCloseEvent(winApiWindow* winApiInst, HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+	{
+		if (message == WM_CLOSE)
+		{
+			if (!DestroyWindow(hWnd))
+			{
+				logLastError("cant destroy window");
+				return false;
+			}
+
+			return true;
+		}
+
+		if (message == WM_DESTROY)
+		{
+			PostQuitMessage(0);
+			winApiInst->mCtx.getDispatcher()->queueEvent(std::make_shared<closeEvent>());
+			return true;
+		}
+
+		return false;
+	}
+
+	bool winApiWindow::handleResizeEvent(winApiWindow* winApiInst, HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+	{
+		if (message == WM_SIZE)
+		{
+			winApiInst->mCtx.getDispatcher()->queueEvent(std::make_shared<windowResizeEvent>(LOWORD(lParam), HIWORD(lParam)));
+			return true;
+		}
+
+		return false;
+	}
+
+	bool winApiWindow::handlePaintEvent(winApiWindow* winApiInst, HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+	{
+		if (message == WM_PAINT)
+			return true;
+
+		return false;
+	}
+
+	LRESULT winApiWindow::WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+	{
+		hwndTableMu.lock();
+		auto pThis = hwndTable.find(hWnd);
+		auto end = hwndTable.end();
+		hwndTableMu.unlock();
+
+		if (pThis != end)
+		{
+			bool handled = false;
+
+			handled |= handleRawInput(pThis->second, hWnd, message, wParam, lParam);
+			handled |= handleResizeEvent(pThis->second, hWnd, message, wParam, lParam);
+			handled |= handlePaintEvent(pThis->second, hWnd, message, wParam, lParam);
+			handled |= handleCloseEvent(pThis->second, hWnd, message, wParam, lParam);
+
+			if (handled)
+				return 0;
+		}
+
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+
+	// Not used right know.
+	void winApiWindow::pollRawInput()
+	{
+		UINT size = 0;
+		if (GetRawInputBuffer(nullptr, &size, sizeof(RAWINPUTHEADER)) == (UINT)-1)
+		{
+			logLastError("GetRawInputBuffer: ");
+		}
+
+		if (size > 0)
+		{
+			auto buffer = alloca(size);
+			PRAWINPUT raw = static_cast<PRAWINPUT>(buffer);
+
+			UINT count = 0;
+
+			if (count = GetRawInputBuffer(raw, &size, sizeof(RAWINPUTHEADER)); count == (UINT)-1)
+			{
+				logLastError("GetRawInputBuffer: ");
+			}
+
+			for (UINT i = 0; i < count; ++i)
+			{
+				if (raw->header.dwType == RIM_TYPEMOUSE) {
+					LOGINFO("Mouse Move: X={}, Y={}", raw->data.mouse.lLastX, raw->data.mouse.lLastY);
+				}
+
+				raw = reinterpret_cast<PRAWINPUT>(
+					reinterpret_cast<BYTE*>(raw) + raw->header.dwSize
+					);
+			}
+		}
+	}
+}
