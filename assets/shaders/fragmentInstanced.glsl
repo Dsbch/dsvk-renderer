77ebacbd0620cf3c8773e76b3c5ef12aea643d91
Азамат Куспанов <azamat.kuspanov@kmf.kz>
2025-06-21 00:45:57 +0500

    upd shader code.

--- assets/shaders/fragmentInstanced.glsl
+++ assets/shaders/fragmentInstanced.glsl
@@ -3,12 +3,19 @@
 uniform sampler2D uAlbedo;
 
 in vsOUT {
+    vec3 fragmentPos;
     vec2 texCoords;
+    vec3 tangentViewPos;
+    vec3 tangentFragmentPos;
 } fsIN;
 
 out vec4 color;
 
 void main()
 {
+    // vec3 normal = texture(uNormal, fs_in.TexCoords).rgb;
+    // this normal is in tangent space, so we use tangentSpace things in fsIN.
+    // normal = normalize(normal * 2.0 - 1.0);
+
     color = texture(uAlbedo, fsIN.texCoords);
 }
--- assets/shaders/vertexInstanced.glsl
+++ assets/shaders/vertexInstanced.glsl
@@ -2,25 +2,40 @@
 
 // Per vertex attrs.
 layout(location = 0) in vec3 position;
-layout(location = 1) in vec2 tex_coords;
+layout(location = 1) in vec2 texCoords;
 layout(location = 2) in vec3 normal;
 layout(location = 3) in vec3 tangent;
 
 // Per instance attr.
-layout(location = 4) in mat4 model_matrix;
+layout(location = 4) in mat4 model;
 
 uniform mat4 uProjection;
 uniform mat4 uView;
 uniform vec3 uViewPos;
 
 out vsOUT {
+    vec3 fragmentPos;
     vec2 texCoords;
+    vec3 tangentViewPos;
+    vec3 tangentFragmentPos;
 } vsOut;
 
 void main()
 {
-    vec4 worldPos = model_matrix * vec4(position.xyz, 1.0f);
-
+    vec4 worldPos = model * vec4(position.xyz, 1.0f);
     gl_Position = uProjection * uView * worldPos;
-    vsOut.texCoords = tex_coords;
+
+    vsOut.texCoords = texCoords;
+    vsOut.fragmentPos = worldPos.xyz;
+
+    // cast to mat3 removes translation.
+    mat3 normalMatrix = transpose(inverse(mat3(model)));
+    vec3 T = normalize(normalMatrix * tangent);
+    vec3 N = normalize(normalMatrix * normal);
+    T = normalize(T - dot(T, N) * N);
+    vec3 B = cross(N, T);
+    mat3 TBN = transpose(mat3(T, B, N));
+
+    vsOut.tangentViewPos = TBN*uViewPos;
+    vsOut.tangentFragmentPos = TBN*vsOut.fragmentPos;
 }
