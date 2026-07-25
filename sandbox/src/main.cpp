76ddf79cd5d61f9ada77bf83d14ca9ab8a79d804
Kuspanov Azamat <orangeking54@gmail.com>
2026-07-25 17:32:46 +0500

    fix local todo

--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -9,10 +9,8 @@
 // 3. Visability buffer.
 // 4. Draw UBO buffer.
 // 5. Pre intsance buffer (transform updates).
-// Separation of game thread and render thread:
-// 1. Game thread is just game logic running all systems. All systems even user systems should run on game thread, they can lauch additional threads but they need to schedule them.
-// 2. Render thread just accepts desired state snapshots produced by game thread and renders. Render thread should be the main thread.
-// 3. Snapshots
+// Write more optimized animation system with co::corutines.
+// Do not block game thread on asset load.
 int main(int argc, char* argv[])
 {
 	flag::parse(argc, argv);
